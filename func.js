2516edd67512d6780e88662cc53638c5a5111004
Juliane Almeida <87144780+JulianeA20@users.noreply.github.com>
2024-03-06 11:05:12 -0300

    Add files via upload

---
+++ func.js
@@ -0,0 +1,33 @@
+const openModalBtn = document.querySelector(".open-modal")
+const closeModalBtn = document.querySelector(".close-modal")
+
+function openModal(fadeClass, modalClass) {
+  const fade = document.querySelector(fadeClass);
+  const modal = document.querySelector(modalClass);
+
+  fade.style.display = 'block';
+  modal.style.display = 'block';
+}
+
+function closeModal(fadeClass, modalClass) {
+  const fade = document.querySelector(fadeClass);
+  const modal = document.querySelector(modalClass);
+
+  fade.style.display = 'none';
+  modal.style.display = 'none';
+}
+
+openModalBtn.addEventListener("click", function() {
+  openModal('.fade', '.modal')
+})
+
+closeModalBtn.addEventListener("click", function() {
+  closeModal('.fade', '.modal')
+})
+
+
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector('#header')
+    header.classList.toggle('rolagem', window.scrollY > 0)
+})

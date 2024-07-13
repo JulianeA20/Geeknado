0ab5a99c4a1844682d2ff50a1ad9eb15db3e433f
Juliane <julyane.almeida77@gmail.com>
2024-07-13 20:18:00 -0300

    segundo-commit

--- func.js
+++ func.js
@@ -1,31 +1,35 @@
-const openModalBtn = document.querySelector(".open-modal")
-const closeModalBtn = document.querySelector(".close-modal")
+const wrapper = document.querySelector(".wrapper")
+const registerLink = document.querySelector('.register-link')
+const loginLink = document.querySelector('.login-link')
+const btnPopup = document.querySelector('.btn-login')
+const iconClose = document.querySelector('.icon-close')
+const overlay = document.querySelector(".overlay")
+const status = document.querySelector(".status")
 
-function openModal(fadeClass, modalClass) {
-  const fade = document.querySelector(fadeClass);
-  const modal = document.querySelector(modalClass);
 
-  fade.style.display = 'block';
-  modal.style.display = 'block';
-}
-
-function closeModal(fadeClass, modalClass) {
-  const fade = document.querySelector(fadeClass);
-  const modal = document.querySelector(modalClass);
-
-  fade.style.display = 'none';
-  modal.style.display = 'none';
-}
-
-openModalBtn.addEventListener("click", function() {
-  openModal('.fade', '.modal')
+registerLink.addEventListener("click", () => {
+  wrapper.classList.add('active')
 })
 
-closeModalBtn.addEventListener("click", function() {
-  closeModal('.fade', '.modal')
+loginLink.addEventListener("click", () => {
+  wrapper.classList.remove('active')
+})
+
+btnPopup.addEventListener("click", () => {
+  wrapper.classList.add('active-popup'),
+  overlay.classList.add('active')
+})
+
+iconClose.addEventListener("click", () => {
+  wrapper.classList.remove('active-popup'),
+  overlay.classList.remove('active')
 })
 
 window.addEventListener("scroll", function(){
-    var header = document.querySelector('#header')
-    header.classList.toggle('rolagem', window.scrollY > 0)
+  var header = document.querySelector('#header')
+  if (window.scrollY > 0) {
+      header.classList.add('rolagem')
+  } else {
+    header.classList.remove('rolagem')
+  }
 })
